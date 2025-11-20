90ff9ce5c8a83d95c5ed67af39259f014567961d
Muhammad Ahmed <ahmdkhn2123@gmail.com>
2025-11-20 14:41:53 +0500

    file name correcte

--- app/Http/Controllers/CRM/UserPostQualificationController.php
+++ app/Http/Controllers/CRM/UserPostQualificationController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Qualification;
 use App\Http\Controllers\Controller;
 use App\Models\UserPostQualification;
-use App\Http\Requests\crm\UserPostQualificationRequest;
+use App\Http\Requests\Crm\UserPostQualificationRequest;
 
 class UserPostQualificationController extends Controller
 {
--- app/Http/Requests/Crm/UserPostQualificationRequest.php
+++ app/Http/Requests/Crm/UserPostQualificationRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\crm;
+namespace App\Http\Requests\Crm;
 
 use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
